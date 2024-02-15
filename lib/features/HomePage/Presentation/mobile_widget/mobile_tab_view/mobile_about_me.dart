4c2748e5bfbb43402fc8b5f3c03967024b842398
Mahesh Babu <mahesh1995.sesetti@gmail.com>
2024-02-15 23:43:27 +0530

    remove container

--- lib/features/HomePage/Presentation/mobile_widget/mobile_tab_view/mobile_about_me.dart
+++ lib/features/HomePage/Presentation/mobile_widget/mobile_tab_view/mobile_about_me.dart
@@ -6,56 +6,52 @@ class AboutMeMobileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.sizeOf(context).height / 1.2,
-       padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-              height: MediaQuery.sizeOf(context).height * 0.3.h,
-              child: Image.asset('assets/images/Frame 20.png')),
-          Text(
-            "Hello I’am Mahesh Babu.\nFlutter Developer\nBased In India.",
-            style: TextStyle(
-              fontSize: 25.sp,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.3.h,
+            child: Image.asset('assets/images/Frame 20.png')),
+        Text(
+          "Hello I’am Mahesh Babu.\nFlutter Developer\nBased In India.",
+          style: TextStyle(
+            fontSize: 25.sp,
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Text(
+            "I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.",
+            style: TextStyle(fontSize: 15.sp, color: Colors.grey[350]),
+          ),
+        ),
+        const SizedBox(
+          height: 40,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {},
+              child: SizedBox.square(
+                  dimension: 40,
+                  child: Image.asset("assets/images/linkedIn.png")),
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Text(
-              "I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.",
-              style: TextStyle(fontSize: 15.sp, color: Colors.grey[350]),
-            ),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              GestureDetector(
-                onTap: () {},
-                child: SizedBox.square(
-                    dimension: 40,
-                    child: Image.asset("assets/images/linkedIn.png")),
-              ),
-              const SizedBox(width: 20,),
-              GestureDetector(
-                onTap: () {},
-                child: SizedBox.square(
-                    dimension: 40,
-                    child: Image.asset("assets/images/github.png")),
-              )
-            ],
-          )
-        ],
-      ),
+            const SizedBox(width: 20,),
+            GestureDetector(
+              onTap: () {},
+              child: SizedBox.square(
+                  dimension: 40,
+                  child: Image.asset("assets/images/github.png")),
+            )
+          ],
+        )
+      ],
     );
   }
 }
